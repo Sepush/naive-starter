1ae32114e12a1f1058dee3bbac94322c3b710d3b
Sepush <sepush@outlook.com>
2022-03-12 16:26:50 +0800

    chore: lint

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import {
-  NSpace,
   NConfigProvider,
   NDialogProvider,
   NLoadingBarProvider,
   NMessageProvider,
   NNotificationProvider,
+  NSpace,
   darkTheme,
 } from 'naive-ui'
 import Home from './components/Home.vue'
@@ -16,15 +16,15 @@ const getDarkTheme = computed(() => isDark.value ? darkTheme : undefined)
 </script>
 
 <template>
-<n-config-provider :theme="getDarkTheme">
-  <n-loading-bar-provider>
-    <n-message-provider>
-      <n-notification-provider>
-        <n-dialog-provider>
-          <Home />
-        </n-dialog-provider>
-      </n-notification-provider>
-    </n-message-provider>
-  </n-loading-bar-provider>
-</n-config-provider>
+  <n-config-provider :theme="getDarkTheme">
+    <n-loading-bar-provider>
+      <n-message-provider>
+        <n-notification-provider>
+          <n-dialog-provider>
+            <Home />
+          </n-dialog-provider>
+        </n-notification-provider>
+      </n-message-provider>
+    </n-loading-bar-provider>
+  </n-config-provider>
 </template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import { NButton, NIcon,NSpace,NLayout } from 'naive-ui'
+import { NButton, NIcon, NLayout, NSpace } from 'naive-ui'
 import { MoonOutline, SunnyOutline } from '@vicons/ionicons5'
 import { isDark, toggleDark } from '../composables'
 </script>
 
 <template>
-<n-space>
-  <n-layout>
-    <div class="page">
-      <n-button @click="toggleDark()">
-        <template #icon>
-          <n-icon v-if="isDark" :component="SunnyOutline" />
-          <n-icon v-else :component="MoonOutline" />
-        </template>
-      </n-button>
-    </div>
-  </n-layout>
-</n-space>
+  <n-space>
+    <n-layout>
+      <div class="page">
+        <n-button @click="toggleDark()">
+          <template #icon>
+            <n-icon v-if="isDark" :component="SunnyOutline" />
+            <n-icon v-else :component="MoonOutline" />
+          </template>
+        </n-button>
+      </div>
+    </n-layout>
+  </n-space>
 </template>
 
 <style>
