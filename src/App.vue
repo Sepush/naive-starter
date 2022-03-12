0ef5416f9a84dc9401188ddebc8d95d7302b858d
Sepush <sepush@outlook.com>
2022-03-12 15:40:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import {
+  NSpace,
+  NConfigProvider,
+  NDialogProvider,
+  NLoadingBarProvider,
+  NMessageProvider,
+  NNotificationProvider,
+  darkTheme,
+} from 'naive-ui'
+import Home from './components/Home.vue'
+import { isDark } from './composables'
+
+const getDarkTheme = computed(() => isDark.value ? darkTheme : undefined)
+</script>
+
+<template>
+<n-config-provider :theme="getDarkTheme">
+  <n-loading-bar-provider>
+    <n-message-provider>
+      <n-notification-provider>
+        <n-dialog-provider>
+          <Home />
+        </n-dialog-provider>
+      </n-notification-provider>
+    </n-message-provider>
+  </n-loading-bar-provider>
+</n-config-provider>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { NButton, NIcon,NSpace,NLayout } from 'naive-ui'
+import { MoonOutline, SunnyOutline } from '@vicons/ionicons5'
+import { isDark, toggleDark } from '../composables'
+</script>
+
+<template>
+<n-space>
+  <n-layout>
+    <div class="page">
+      <n-button @click="toggleDark()">
+        <template #icon>
+          <n-icon v-if="isDark" :component="SunnyOutline" />
+          <n-icon v-else :component="MoonOutline" />
+        </template>
+      </n-button>
+    </div>
+  </n-layout>
+</n-space>
+</template>
+
+<style>
+.page {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  text-align: center;
+}
+</style>
