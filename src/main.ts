d89c77ce6e30ff2875159da4fac7881b7beefdeb
Sepush <sepush@outlook.com>
2024-04-20 17:16:30 +0800

    fix: preflight

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'uno.css'
 
+const meta = document.createElement('meta')
+meta.name = 'naive-ui-style'
+document.head.appendChild(meta)
 
 createApp(App).mount('#app')
